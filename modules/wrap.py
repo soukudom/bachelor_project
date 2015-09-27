ae96d13daf86f89023e71d1fbea88691abe98b13
Dominik Soukup <soukupdo@gmail.com>
2015-09-27 19:17:30 +0200

    Submethod parsing detection.
    I added submethod feature in configuration file.

--- modules/wrap.py
+++ modules/wrap.py
@@ -48,127 +48,100 @@ class parseConfig:
         self.groupName = ""
         self.className = ""
         self.methodName = ""
+        self.subMethodName = ""
 
-        self._rekurze(self.data, False, False, False)
+        self._rekurze(self.data, False, False, False,False,0)
         sys.exit(1)
-        for i in self.data:
-            if list(i.keys())[0] == "group":
-            # najdi si skupinu
-            # dej mi jmeno skupiny
-                self.groupName = i["group"][0]["name"]
-                del i["group"][0]["name"]
-                for j in i["group"]:
-                        
-                    #ziskej jmena trid 
-                    try:
-                        if list(j.keys())[0] == "group":
-                            #zpracuj skupinu
-                            pass
-                        else:
-                            
-                            self.className = list(j.keys())[0]
-                            print("jmeno tridy:",self.className)
-                            for k in j[self.className]:
-                                print(k)
-                    except IndexError:
-                        continue
-                    except Exception as e:
-                        print(e)
-            else:
-                pass
-            #pokracuj pro vsechny
-            sys.exit(1)
-    # vymazavat promenny jmena a projit si az do konce
-    def _rekurze(self, data, group, class_, method):
-        groupLevel = group
-        group = False
-        class_ = class_
-        method = method
+
+    def _rekurze(self, data, group, class_, method, subMethod,groupNum):
+        groupLevel = group # flag, ktery rika jestli mam odpojovat skupinu
+        groupNumber = groupNum # pocet prvku skupiny, pro odebirani
+        class_ = class_ # flag, ktery rika jestli vyskakuju ze tridy
+        method = method # flag, ktery rika jeslti vyskakuju z metody
+        subMethod = subMethod # flag, ktery mi rika jestli vyskakuju z podmetody
         
         
         for i in data:
-            print("hodnoty:",groupLevel, class_,method)
-            #if class_:
-            #    print("resetuju class a na zacatku")
-            #    self.className = ""
-            #elif method:
-            #    print("resetuju method na zacatku")
-            #    self.methodName = ""
-
-            print("prvni prvek z foru:", i)
+            #print("prvni prvek z foru:", i)
             input("stiskni enter")
             try:
                 if type(i) == type(str()):
-                    #method = True
-                    print("dosel jsem ke konci")
+                    #print("dosel jsem ke konci")
+                    print("klic:",i," hodnota:",data[i])
+                    if type(data[i]) == type(dict()):
+                        if self.subMethodName == "":
+                            self.subMethodName = self.methodName+"_"+str(i)
+                        else:
+                            self.subMethodName = self.subMethodName+"_"+str(i)
+                        print("nasel jse m submethod",self.subMethodName)
+                        self._rekurze(data[i],False,False,False,True,groupNumber)
                     #for m in data.values():
                     #   print(m,type(m))
-                    print(i)
                     continue
-                #try:
                 elif list(i.keys())[0] == "group":
-    # pridavani jmena grupy dobry, ale odebirani jmena udelat podle posledniho jmena v parametru name, tzn odebrat treba vic levelu najednou jako je u sin:48
 
                     if self.groupName == "":
-                        print("nastavuju group 1")
-                        self.groupName = i["group"][0]["name"]
-                        group = True
-                    #elif group:
-                    #    print("nastavuju group 2")
-                    #    self.groupName = self.groupName.rsplit(":",1)[0]
-                    #    self.groupName += ":"+str(i["group"][0]["name"])
+                     #   print("nastavuju group 1")
+                        self.groupName = str(i["group"][0]["name"])
                     else:
-                        print("nastavuju group 3")
+                      #  print("nastavuju group 3")
                         self.groupName += ":"+str(i["group"][0]["name"])
-                        group = True
 
+                    groupNumber = len(str(i["group"][0]["name"]).split(":"))
                     del i["group"][0]["name"]
-                    print("nasel jsem group",self.groupName ,"dalsi kolo:",i["group"])
-                        #group = True
+                    #print("nasel jsem group",self.groupName ,"dalsi kolo:",i["group"])
+                    print("nasel jsem group",self.groupName)
                     input("stiskni enter")
-                    self._rekurze(i["group"],True,False,False) 
-                #except IndexError:
-                #   continue
+                    self._rekurze(i["group"],True,False,False,False,groupNumber) 
                 else:
                     if self.className == "":
                         self.className = list(i.keys())[0]
-                        print("nasel jsem tridu",self.className ," dalsi kolo:",i[self.className])
+                        #print("nasel jsem tridu",self.className ," dalsi kolo:",i[self.className])
+                        print("nasel jsem tridu",self.className)
                         input("stiskni enter")
                         #class_ = True
-                        self._rekurze(i[self.className],False,True,False)
+                        self._rekurze(i[self.className],False,True,False,False,groupNumber)
 
                     elif self.methodName == "":
                         self.methodName = list(i.keys())[0]
-                        print("nasel jsem metodu",self.methodName," dalsi kolo:",i[self.methodName])
-                        #method = True
+                        #print("nasel jsem metodu",self.methodName," dalsi kolo:",i[self.methodName])
+                        print("nasel jsem metodu",self.methodName)
                         input("stiskni enter")
-                        #print("hodnoty", group,class_,method)
-                        self._rekurze(i[self.methodName],False,False,True)
+                        self._rekurze(i[self.methodName],False,False,True,False,groupNumber)
             except IndexError:
                 continue
             
-                #elif True:
-                #    print(i)
-                #    print("jsem na konci")
-                #    continue
         print("jdu pryc z foru", groupLevel, class_, method)
         if class_:
-            print("resetuju class a vracim se")
+            #print("resetuju class a vracim se")
             self.className = ""
             class_ = False
             return
         elif method:
-            print("resetuju method a vracim se")
+            #print("resetuju method a vracim se")
             self.methodName = ""
             method = False
             #class_ = True
+            print("moje zkontrolovana data jsou:" )
+            for k in data:
+                print(k,":",data[k])
             return
         elif groupLevel:
-            tmp = self.groupName.rsplit(":",1)
-            if len(tmp) == 1:
+            #print("cislo skupiny",groupNumber)
+            tmp = self.groupName.rsplit(":",groupNumber)
+            if len(tmp) == 1 or groupNumber == len(self.groupName.split(":")):
                 self.groupName = ""
             else:
-                self.groupName = self.groupName.rsplit(":",1)[0]
-            print("menim groupName na:", self.groupName) 
+                self.groupName = tmp[0] 
+            #print("menim groupName na:", self.groupName) 
+        elif subMethod:
+            tmp = self.subMethodName.rsplit("_",1)
+            if len(tmp) == 1:
+                self.subMethodName = ""
+            else:
+                self.subMethodName = tmp[0]
+            subMethod = False
+            
+            
                 
  
