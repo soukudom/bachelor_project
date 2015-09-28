0d2e0b2c44c646a995d8308b1a4a134d8bc418b9
Dominik Soukup <soukupdo@gmail.com>
2015-09-28 15:31:37 +0200

    Unpacking feature created.
    I add new method for unpack shortening value in configure file.

--- modules/wrap.py
+++ modules/wrap.py
@@ -3,6 +3,8 @@
 
 import yaml
 import sys
+import re
+from itertools import product
 
 class parseDevice:
     def __init__(self, filename, group):
@@ -50,15 +52,54 @@ class parseConfig:
         self.methodName = ""
         self.subMethodName = ""
 
-        self._rekurze(self.data, False, False, False,False,0)
+        self._rekurze(self.data, False, False, False,False,0,None)
         sys.exit(1)
 
-    def _rekurze(self, data, group, class_, method, subMethod,groupNum):
+    def _checkId(self,name):
+    # dodelat detekci range u jmena, pac pouze cislo nestaci
+
+        buffer = []
+        for i in reversed(name):
+            try:
+                int(i)
+                buffer.insert(0,i)
+            except Exception as e:
+                print(e)
+                tmp = re.search("\([1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*\)$",name)
+                print("vysledek z re je", tmp)
+                if tmp is not None:
+                    buffer = tmp.group(0).strip("()").split(",")
+                    return buffer
+                break
+        
+        if not buffer:
+            return False
+        else:
+            return int("".join(buffer))
+    def _unpack(self, value):
+        if re.match("\([1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*\)",value):
+            return value.group(0).strip("()").split(",")
+        elif re.search("\([1-9][0-9]*,[1-9][0-9]*,[1-9][0-9]*\)",value):         
+            buffer = []
+            ret = []
+            tmp = re.split("\(|\)",value)
+            for i in tmp[:-1]:
+                if "," in i:
+                    buffer.append(i.split(","))
+                else:
+                    buffer.append([i])
+            for i in product(*buffer):
+                ret.append(''.join(i))
+            return ret
+
+    def _rekurze(self, data, group, class_, method, subMethod,groupNum,idNum):
         groupLevel = group # flag, ktery rika jestli mam odpojovat skupinu
         groupNumber = groupNum # pocet prvku skupiny, pro odebirani
         class_ = class_ # flag, ktery rika jestli vyskakuju ze tridy
         method = method # flag, ktery rika jeslti vyskakuju z metody
         subMethod = subMethod # flag, ktery mi rika jestli vyskakuju z podmetody
+        #idNum promena, ktera obsahuje identifikator rozhrani
+        #mohl bych ji sem taky pro prehlednost pridat
         
         
         for i in data:
@@ -74,7 +115,7 @@ class parseConfig:
                         else:
                             self.subMethodName = self.subMethodName+"_"+str(i)
                         print("nasel jse m submethod",self.subMethodName)
-                        self._rekurze(data[i],False,False,False,True,groupNumber)
+                        self._rekurze(data[i],False,False,False,True,groupNumber,None)
                     #for m in data.values():
                     #   print(m,type(m))
                     continue
@@ -92,7 +133,7 @@ class parseConfig:
                     #print("nasel jsem group",self.groupName ,"dalsi kolo:",i["group"])
                     print("nasel jsem group",self.groupName)
                     input("stiskni enter")
-                    self._rekurze(i["group"],True,False,False,False,groupNumber) 
+                    self._rekurze(i["group"],True,False,False,False,groupNumber,None) 
                 else:
                     if self.className == "":
                         self.className = list(i.keys())[0]
@@ -100,14 +141,18 @@ class parseConfig:
                         print("nasel jsem tridu",self.className)
                         input("stiskni enter")
                         #class_ = True
-                        self._rekurze(i[self.className],False,True,False,False,groupNumber)
+                        #idNum = self._checkId(self.className)
+                        #print("checkId vratilo", idNum)
+                        self._rekurze(i[self.className],False,True,False,False,groupNumber,None)
 
                     elif self.methodName == "":
                         self.methodName = list(i.keys())[0]
                         #print("nasel jsem metodu",self.methodName," dalsi kolo:",i[self.methodName])
                         print("nasel jsem metodu",self.methodName)
                         input("stiskni enter")
-                        self._rekurze(i[self.methodName],False,False,True,False,groupNumber)
+                        idNum = self._checkId(self.methodName)
+                        print("checkId vratilo", idNum)
+                        self._rekurze(i[self.methodName],False,False,True,False,groupNumber,idNum)
             except IndexError:
                 continue
             
@@ -125,6 +170,7 @@ class parseConfig:
             print("moje zkontrolovana data jsou:" )
             for k in data:
                 print(k,":",data[k])
+            print("identifikator: ",idNum)
             return
         elif groupLevel:
             #print("cislo skupiny",groupNumber)
